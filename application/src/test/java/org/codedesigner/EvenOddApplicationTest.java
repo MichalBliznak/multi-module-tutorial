b4a6316c025bb5befdf72ad5ea1576a5aecd0deb
Michal Bližňák <michal.bliznak@gmail.com>
2019-06-13 23:59:49 +0200

    Improved unit tests.

--- application/src/test/java/org/codedesigner/EvenOddApplicationTest.java
+++ application/src/test/java/org/codedesigner/EvenOddApplicationTest.java
@@ -3,13 +3,13 @@ package org.codedesigner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertNotNull;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {EvenOddApplication.class})
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { EvenOddApplication.class })
 public class EvenOddApplicationTest {
 
     @Autowired
--- library/src/main/java/org/codedesigner/EvenOddServiceConfig.java
+++ library/src/main/java/org/codedesigner/EvenOddServiceConfig.java
@@ -1,11 +1,13 @@
 package org.codedesigner;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@EnableAutoConfiguration
+@EnableConfigurationProperties
 @Configuration
 @ComponentScan(basePackages = {"org.codedesigner"})
-@EnableConfigurationProperties
 public class EvenOddServiceConfig {
 }
