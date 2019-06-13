64ab2a583ff2545b916c4688ce5b1c8bbcaa10c3
Michal Bližňák <michal.bliznak@gmail.com>
2019-06-13 22:43:03 +0200

    Improved library unit tests.

--- library/src/test/java/org/codedesigner/EvenOddServiceConfigTest.java
+++ library/src/test/java/org/codedesigner/EvenOddServiceConfigTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,6 +19,16 @@ public class EvenOddServiceConfigTest {
     @Test
     public void evenOddServiceIsLoaded() {
         assertNotNull(evenOddService);
+    }
+
+    @Test
+    public void isOddNumber() {
+        assertEquals(evenOddService.isEvenOrOdd(1), "Odd");
+    }
+
+    @Test
+    public void isEvenNumber() {
+        assertEquals(evenOddService.isEvenOrOdd(2), "Even");
     }
 
 }
