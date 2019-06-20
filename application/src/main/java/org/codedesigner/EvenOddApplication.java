f92151bc4a3e27f90355fd1d6c7b85a2d2bc6317
Michal Bližňák <michal.bliznak@gmail.com>
2019-06-20 21:53:21 +0200

    Removed un-needed annotations.

--- application/src/main/java/org/codedesigner/EvenOddApplication.java
+++ application/src/main/java/org/codedesigner/EvenOddApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(scanBasePackages = "org.codedesigner")
+@SpringBootApplication
 @RestController
 public class EvenOddApplication {
 
--- library/src/main/java/org/codedesigner/EvenOddServiceConfig.java
+++ library/src/main/java/org/codedesigner/EvenOddServiceConfig.java
@@ -1,12 +1,8 @@
 package org.codedesigner;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@EnableAutoConfiguration
-@EnableConfigurationProperties
 @Configuration
 @ComponentScan(basePackages = {"org.codedesigner"})
 public class EvenOddServiceConfig {
